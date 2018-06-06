35c248ada52b00686a73d4b2ea9a71c307c1383e
EvilMealw0rm <evilmealw0rm@gmx.at>
2018-06-06 21:24:59 +0200

    Camera in 00_empty implemented

--- 00_empty/main.js
+++ 00_empty/main.js
@@ -5,12 +5,21 @@ var canvasWidth = 1000;
 var canvasHeight = 800;
 var aspectRatio = canvasWidth / canvasHeight;
 
-const camera = {
-  rotation: {
-    x: 0,
-    y: 0
-  }
-};
+// camera values
+var cameraPos = vec3.fromValues(0, 0, 3);
+var cameraFront = vec3.fromValues(0, 0, -1);
+var cameraUp = vec3.fromValues(0, 1, 0);
+var cameraRight = vec3.fromValues(0, 0, 0);
+var pitch = 0;
+var yaw = -90;
+var speed = 0.2;
+var sensitivity = 0.02;
+
+var timeSinceLastFrame;
+
+// constant upvector
+var upvector = vec3.fromValues(0, 1, 0);
+
 //rendering context
 var context;
 
@@ -292,6 +301,7 @@ function makeCube(width, height, length){
   index: index
   };
 }
+
 function createRobot(rootNode) {
   cubeNode = new MaterialNode([new RenderSGNode(makeCube(2,2,2))]);
   cubeNode.ambient = [0.24725, 0.1995, 0.0745, 1];
@@ -376,16 +386,20 @@ function render(timeInMilliseconds) {
   const context = createSGContext(gl);
   context.projectionMatrix = mat4.perspective(mat4.create(), glm.deg2rad(30), gl.drawingBufferWidth / gl.drawingBufferHeight, 0.01, 100);
 
+  let lookatVec = vec3.add(vec3.create(), cameraPos, cameraFront);
+  let lookAtMatrix = mat4.lookAt(mat4.create(), cameraPos, lookatVec, cameraUp);
   //ReCap: what does this mean?
-  context.viewMatrix = mat4.lookAt(mat4.create(), [0,1,-10], [0,0,0], [0,1,0]);
+  context.viewMatrix = lookAtMatrix;
+
+  context.timeInMilliseconds = timeInMilliseconds;
 
   //TASK 0-2 rotate whole scene according to the mouse rotation stored in
   //camera.rotation.x and camera.rotation.y
-  context.sceneMatrix = mat4.create();
+  //context.sceneMatrix = mat4.create();
   //robotTransformationNode.matrix = glm.rotateY(animatedAngle/2);
-  context.sceneMatrix = mat4.multiply(mat4.create(),
-                            glm.rotateY(camera.rotation.x),
-                            glm.rotateX(camera.rotation.y));
+  //context.sceneMatrix = mat4.multiply(mat4.create(),
+  //                          glm.rotateY(camera.rotation.x),
+  //                          glm.rotateX(camera.rotation.y));
   root.render(context);
 
   //request another render call as soon as possible
@@ -525,6 +539,7 @@ class MaterialNode extends SGNode {
   }
 }
 
+// control user input
 function initInteraction(canvas) {
   const mouse = {
     pos: { x : 0, y : 0},
@@ -545,11 +560,9 @@ function initInteraction(canvas) {
   canvas.addEventListener('mousemove', function(event) {
     const pos = toPos(event);
     const delta = { x : mouse.pos.x - pos.x, y: mouse.pos.y - pos.y };
-    //TASK 0-1 add delta mouse to camera.rotation if the left mouse button is pressed
     if (mouse.leftButtonDown) {
       //add the relative movement of the mouse to the rotation variables
-  		camera.rotation.x += delta.x;
-  		camera.rotation.y += delta.y;
+  		moveWithMouse(delta.x, delta.y);
     }
     mouse.pos = pos;
   });
@@ -560,9 +573,93 @@ function initInteraction(canvas) {
   //register globally
   document.addEventListener('keypress', function(event) {
     //https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
-    if (event.code === 'KeyR') {
-      camera.rotation.x = 0;
-  		camera.rotation.y = 0;
+
+    console.log(event.code + " key pressed");
+
+    let velocity = 1;
+    switch (event.code) {
+      case "KeyR":
+        resetCamera();
+        break;
+      case "KeyW":
+      case "ArrowUp":
+      case "Numpad8":
+        moveForward(velocity);
+        break;
+      case "KeyA":
+      case "ArrowLeft":
+      case "Numpad4":
+        strafeLeft(velocity);
+        break;
+      case "KeyS":
+      case "ArrowDown":
+      case "Numpad2":
+        moveBackward(velocity);
+        break;
+      case "KeyD":
+      case "ArrowRight":
+      case "Numpad6":
+        strafeRight(velocity);
+        break;
+      default:
+
     }
   });
+}
+
+function updateCameraVectors() {
+  // calc new front vec
+  cameraFront[0] = Math.cos(glm.deg2rad(yaw)) * Math.cos(glm.deg2rad(pitch));
+  cameraFront[1] = Math.sin(glm.deg2rad(pitch));
+  cameraFront[2] = Math.sin(glm.deg2rad(yaw)) * Math.cos(glm.deg2rad(pitch));
+  vec3.normalize(cameraFront, cameraFront);
+  // recalculate right- and upvector
+  vec3.normalize(cameraRight, vec3.cross(vec3.create(), cameraFront, upvector));
+  vec3.normalize(cameraUp, vec3.cross(vec3.create(), cameraRight, cameraFront));
+}
+
+function resetCamera() {
+  setCameraPosAndLookAt(vec3.fromValues(0, -2, -10), vec3.fromValues(0, 0, 0));
+  pitch = 0;
+  yaw = -90;
+}
+
+function moveWithMouse(deltaX, deltaY) {
+  deltaX *= sensitivity;
+  deltaY *= sensitivity;
+
+  yaw += deltaX;
+  pitch -= deltaY;
+
+  // Makes sure that the screen won't flip if the pitch is out of bounds
+  if (pitch > 89) {
+    pitch = 89;
+  }
+  if (pitch < -89) {
+    pitch = -89;
+  }
+
+  updateCameraVectors();
+}
+
+function moveForward(velocity) {
+  vec3.add(cameraPos, cameraPos, cameraFront);
+}
+
+function moveBackward(velocity) {
+  vec3.sub(cameraPos, cameraPos, cameraFront);
+}
+
+function strafeLeft(velocity) {
+  vec3.sub(cameraPos, cameraPos, cameraRight);
+}
+
+function strafeRight(velocity) {
+  vec3.add(cameraPos, cameraPos, cameraRight);
+}
+
+function setCameraPosAndLookAt(toPos, toLook) {
+  cameraPos = toPos;
+  cameraUp = vec3.fromValues(0, 1, 0);
+  vec3.add(cameraFront, cameraPos, toLook);
 }
