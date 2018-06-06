6ad02372d5a4350b7a15199e30e5d4aa95ba2c0c
DESKTOP-V6HU8MS\edith <kardaedith98@gmail.com>
2018-06-06 11:38:42 +0200

    Added simple robot.

---
+++ 05_texturing/shader/empty.fs.glsl
@@ -0,0 +1,14 @@
+/**
+ * empty basic fragment shader
+ */
+
+//need to specify how "precise" float should be
+precision mediump float;
+
+varying vec3 v_color;
+uniform float u_alpha;
+
+//entry point again
+void main() {
+  gl_FragColor = vec4(v_color, u_alpha);
+}
